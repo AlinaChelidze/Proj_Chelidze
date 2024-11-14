892f5bbdb2ab4d491464a351e8a618bd5e30f282
AlinaChelidze <alina0904alina@icloud.com>
2024-11-14 21:05:58 +0300

    Добавление контента

---
+++ PZ_4/PZ_4_1.py
@@ -0,0 +1,19 @@
+"""
+Вариант 27.
+1. Дано целое число N (>0). Найти сумму 1 + 1/2 + 1/3 + ... + 1/N
+"""
+
+def sum_of_fractions(N):
+
+  if N <= 1:
+      return "N должно быть больше 1"
+
+  sum = 0
+  for i in range(1, N + 1):
+    sum += 1 / i
+  return sum
+
+
+N = 10
+sum1 = sum_of_fractions(N)
+print(f"Сумма для N = {N}: {sum1}")
